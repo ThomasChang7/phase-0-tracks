60ed0d53423889f06262602f72bcb40336aa5b4e
Thomas Chang <Tomchang93@gmail.com>
2017-01-12 22:42:49 -0500

    implemented simple search algorithm

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,15 @@
+# Release 0: Implement a Simple Search
+def search_array(arr, number)
+	i = 0
+  while i < arr.length
+		if arr[i] == number
+    	return i
+    end
+	i += 1
+  end
+end
+
+# Release 1: Calculate Fibonacci Numbers
+
+
+# Release 2: Sort an Array
