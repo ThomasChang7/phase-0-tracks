54a1a8f11587936b9bc4af6ba4e6fb5983a5155b
Thomas Chang <Tomchang93@gmail.com>
2017-01-13 17:44:41 -0500

    5.6 commit in progress pairing session

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -13,3 +13,63 @@ end
 
 
 # Release 2: Sort an Array
+
+
+def search(array, num)
+  i = 0
+  while i < array.length
+    if array[i] == num
+      return i
+    end
+    i += 1
+  end
+end
+
+
+
+def fib(num)
+arr = [0,1]
+i = 0
+  while i < num - 2
+    arr << arr[-1] + arr[-2]
+    i += 1
+  end
+arr.last
+end
+
+# Release 0: Implement a Simple Search
+
+def search_array(arr, number)
+	i = 0
+  while i < arr.length
+		if arr[i] == number
+    	return i
+    end
+	i += 1
+  end
+end
+
+# Release 1: Calculate Fibonacci Numbers
+
+def fibonacci(number)
+	fib = [0, 1]
+  (numbers - 2).times do |x|
+  	fib << fib[-2] + fib[-1]
+  put fib
+end
+
+def fib(num)
+arr = [0,1]
+i = 0
+  while i < num - 2
+    arr << arr[-1] + arr[-2]
+    i += 1
+  end
+arr.last
+end
+
+  https://projecteuler.net/
+
+fibonacci(100) == 218922995834555169026
+
+# Release 2: Sort an Array
