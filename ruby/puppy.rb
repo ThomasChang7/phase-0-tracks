dcea113dd2b0974ebd39079b83bc0bd7d6d4ed09
Thomas Chang <Tomchang93@gmail.com>
2017-01-17 12:21:26 -0500

    wrote readable code and defined a simple class

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -8,7 +8,20 @@
 # Intelligence: Smart
 # Affection: Five Stars
 
-# Behavior:
-# Friendly
-# Loyal
-# Playful
+# Tricks:
+# Play dead
+# Howl
+# Sit
+# Roll Over
+
+class Puppy
+end
+
+# elf = Puppy.new
+# balu = Puppy.new
+# bucky = Puppy.new
+
+# elf.class => Puppy
+# elf.instance_of?(Puppy) => true
+# elf.bark is an instance method because it is being called on an instance.
+# Puppy.methods would be a class method because it is being called upon a class.
