297486cc0ae3471d490cc79f9f6f3f11ec92dd92
Thomas Chang <Tomchang93@gmail.com>
2017-01-17 12:10:39 -0500

    designed a puppy class in comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# Puppy Class Design
+# Species: German Shepherd
+
+# Characteristics:
+# Temperament: Varies
+# Eye Count: 2
+# Legs: 4
+# Intelligence: Smart
+# Affection: Five Stars
+
+# Behavior:
+# Friendly
+# Loyal
+# Playful
