aa813795e637877056c8683b4c73e2fb39009c1b
Thomas Chang <Tomchang93@gmail.com>
2017-01-18 16:37:16 -0500

    updated Puppy class and added Kitten class for 6.2, paired with Natalie Ruiz

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,70 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times { puts "Woof!"}
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(age)
+      puts age * 7
+  end
+
+  def bang
+    puts "*play dead*"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+
 end
 
+class Kitten
+
+  def initialize
+    puts "Initializing new kitten instance...meow meow...meow"
+  end
+
+  def go_crazy(int)
+    int.times {puts "*runs back and forth in the room*" }
+  end
+
+  def bother(object)
+    puts "Lies down on #{object} while you are trying to do work."
+  end
+
+end
+
+
+
+
+
+# driver code
+tootsie = Puppy.new
+
+
+tootsie.fetch("bone toy")
+tootsie.speak(4)
+tootsie.roll_over
+tootsie.dog_years(3)
+tootsie.bang
+
+
+# make 50 instances of Kitten class
+counter = 0
+kittenz = []
+until counter == 50
+  kittenz << Kitten.new
+  counter += 1
+end
+
+# p kittenz
+
+kittenz.each do |kitten|
+  kitten.go_crazy(2)
+  kitten.bother("keyboard")
+end
