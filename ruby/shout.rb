7373aecc41a0c3084a4884856c1629f3ffef0e85
Thomas Chang <Tomchang93@gmail.com>
2017-01-20 00:52:31 -0500

    Converted a standalone module to a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,36 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#
+#   def self.yelling_happily(words)
+#     words + "!" + " :D"
+#   end
+# end
+#
+# p Shout.yell_angrily("No!")
+# p Shout.yelling_happily("Hi There")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def shout(call)
+    p "#{call.upcase}!!! DROP AND GIVE ME 20"
   end
 
-  def self.yelling_happily(words)
-    words + "!" + " :D"
+  def cranky(call)
+    p "HEY #{call.upcase}!!! GET OFF MY PROPERTY *cough cough*"
   end
 end
 
-p Shout.yell_angrily("No!")
-p Shout.yelling_happily("Hi There")
+class Coach
+  include Shout
+end
+
+class Old_Man
+  include Shout
+end
+
+coach = Coach.new
+coach.shout("stop goofing around")
+
+old_man = Old_Man.new
+old_man.cranky("whippersnapper")
