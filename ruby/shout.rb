b6dacf0ac1bcdddcdd0cba23f49b61326d6ca5bb
Thomas Chang <Tomchang93@gmail.com>
2017-01-20 00:42:14 -0500

    defined two class methods yell angrily and yelling happily using self

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
 
+  def self.yelling_happily(words)
+    words + "!" + " :D"
+  end
 end
+
+p Shout.yell_angrily("No!")
+p Shout.yelling_happily("Hi There")
