2c28c32523ef2967fdfe74fe2e3623a75fb0b4ee
Thomas Chang <Tomchang93@gmail.com>
2017-02-09 11:57:27 -0500

    did releases 0-2 and built two new forms

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -8,8 +8,21 @@ db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
 # show students on the home page
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS campuses(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
+  )
+SQL
+# post '/campuses' do
+db.execute(create_table_cmd)
+#   db.execute("INSERT INTO campuses (name) VALUES (?)", [params['name']])
+#   redirect '/'
+# end
+
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  @campuses = db.execute("SELECT * FROM campuses")
   erb :home
 end
 
@@ -23,5 +36,29 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+
+get '/students/remove' do 
+  erb :remove_student
+end
+
+# delete a student via form
+post '/students_rm' do
+  db.execute("DELETE FROM students WHERE name=?", [params['name']])
+  redirect '/'
+end
+
+post '/campuses' do
+  db.execute("INSERT INTO campuses (name) VALUES (?)", [params['name']])
+  redirect '/'
+end
+
+get '/campuses/new' do
+  erb :new_campus
+end
+
+
+
+
 
 # add static resources
